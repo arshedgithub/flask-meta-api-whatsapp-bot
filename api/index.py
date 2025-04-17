ec7fc2193254b36be431950306db55d24fd93354
arshedgithub <arshedahmedmail@gmail.com>
2025-04-17 22:26:12 +0530

    initial route with env var

--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+import os
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(__name__)
 
+TOKEN = os.getenv("WHATSAPP_TOKEN")
+PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
+VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'Whatsapp Bot deployed Successfully!'
 
-@app.route('/about')
-def about():
-    return 'About'
