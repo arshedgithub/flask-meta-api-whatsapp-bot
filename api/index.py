e5c3024394c9772eea33bc88b2f2c8ddf3c0ac4a
arshedgithub <arshedahmedmail@gmail.com>
2025-04-17 22:33:58 +0530

    sending single message

--- api/index.py
+++ api/index.py
@@ -9,9 +9,29 @@ app = Flask(__name__)
 
 TOKEN = os.getenv("WHATSAPP_TOKEN")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
-VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
+RECIPIENT_PHONE_NUMBER = os.getenv("RECIPIENT_PHONE_NUMBER")
+# VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 
 @app.route('/')
 def home():
     return 'Whatsapp Bot deployed Successfully!'
+
+url = f"https://graph.facebook.com/v18.0/{PHONE_NUMBER_ID}/messages"
+    
+headers = {
+    "Authorization": f"Bearer {TOKEN}",
+    "Content-Type": "application/json"
+}
+message = "Hello! Whatsapp Bot was build..."
+data = {
+    "messaging_product": "whatsapp",
+    "to": RECIPIENT_PHONE_NUMBER,
+    "type": "text",
+    "text": {
+        "body": message
+    }
+}
+
+response = requests.post(url, headers=headers, json=data)
+print(response.status_code, response.text)
 
