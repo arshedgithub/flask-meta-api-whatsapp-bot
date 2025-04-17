99a40e85d315be7117b40ba872901ff2e71fb525
arshedgithub <arshedahmedmail@gmail.com>
2025-04-17 23:12:55 +0530

    sending messages

--- api/index.py
+++ api/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import os
 import requests
 from dotenv import load_dotenv
@@ -16,25 +16,6 @@ VERIFY_TOKEN = os.getenv("VERIFY_TOKEN")
 def home():
     return 'Whatsapp Bot deployed Successfully!'
 
-# url = f"https://graph.facebook.com/v18.0/{PHONE_NUMBER_ID}/messages"
-    
-# headers = {
-#     "Authorization": f"Bearer {TOKEN}",
-#     "Content-Type": "application/json"
-# }
-# message = "Hello! Whatsapp Bot was build..."
-# data = {
-#     "messaging_product": "whatsapp",
-#     "to": RECIPIENT_PHONE_NUMBER,
-#     "type": "text",
-#     "text": {
-#         "body": message
-#     }
-# }
-
-# response = requests.post(url, headers=headers, json=data)
-# print(response.status_code, response.text)
-
 # webhook verification route 
 @app.route('/webhooks', methods=['GET'])
 def verify():
@@ -49,6 +30,47 @@ def verify():
         else:
             return "Forbidden", 403
         
+# listening
+@app.route('/webhooks', methods=['POST'])
+def receive_message():
+    data = request.get_json()
+    print("Received webhook:", data)
+
+    try:
+        message = data['entry'][0]['changes'][0]['value']['messages'][0]
+        phone_number = message['from']
+        text = message['text']['body'].strip().lower()
+
+        if text in ["hi", "hello"]:
+            reply_text = "Hi there! 👋 How can I help you today?"
+            send_whatsapp_message(phone_number, reply_text)
+        else:
+            send_whatsapp_message(phone_number, "Hello! I am whastapp Bot. Ask any Question!")
+            
+    except Exception as e:
+        print("No text message or error:", e)
+
+    return jsonify(success=True)
+
+
+def send_whatsapp_message(to, message):
+    url = f"https://graph.facebook.com/v18.0/{PHONE_NUMBER_ID}/messages"
+    headers = {
+        "Authorization": f"Bearer {TOKEN}",
+        "Content-Type": "application/json"
+    }
+    data = {
+        "messaging_product": "whatsapp",
+        "to": to,
+        "type": "text",
+        "text": {
+            "body": message
+        }
+    }
+    
+    response = requests.post(url, headers=headers, json=data)
+    print(response.status_code, response.text)
+        
+        
 if __name__ == "__main__":
     app.run(debug=True)
-        
